24b9c26b717efdae08b21ea9981538bb4ee72068
“david“ <david@largitdata.com>
2020-03-24 14:13:44 +0800

    add tutorial

---
+++ tutorial/tutorial/spiders/crawl.py
@@ -0,0 +1,10 @@
+import scrapy
+
+class MoneyCrawler(scrapy.Spider):
+    name = "money"
+    start_urls = ['https://money.udn.com/rank/newest/1001/0/1']
+    def parse(self, response):
+        for tr in response.css('#ranking_table tr'):
+            if tr.css('td'):
+                url = tr.css('a::attr(href)')[0].extract()
+                print(url)
